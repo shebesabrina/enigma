01f804fb5aae5e5037697972c3e1d61d3d2fdb55
Lihkin <shniks@gmail.com>
2018-02-05 01:46:42 -0700

    Adding additional tests for the decrypt method and completing decrypt method.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -53,4 +53,46 @@ class EnigmaTest < Minitest::Test
     @e.encrypt(my_message, 30218, "12345")
   end
 
+  def test_if_decrypt_method_returns_a_string
+    assert_equal String, @e.decrypt("hello world", "30218", "12345").class
+  end
+
+  def test_it_can_decrypt_a_letter
+    my_message = "k..end.."
+
+    assert_equal "39O!6bOO", @e.decrypt(my_message, "30218", "12345")
+  end
+
+  def test_it_can_decrypt_a_second_letter
+    my_message = "ki..end.."
+
+    assert_equal "3gOOXl,Oy", @e.decrypt(my_message, "30218", "12345")
+  end
+
+  def test_it_can_decrypt_a_four_letter_word
+    my_message = "bell..end.."
+
+    assert_equal "Uc**y9!)W9O", @e.decrypt(my_message, "30218", "12345")
+  end
+
+  def test_it_can_decrypt_two_words
+    my_message = "bell hop..end.."
+
+    assert_equal "Uc**xf[]y9!)W9O", @e.decrypt(my_message, "30218", "12345")
+  end
+
+  def test_it_can_decrypt_words_with_a_comma
+    my_message = "the answer is, 3..end.."
+
+    assert_equal ",f!NTl;|XpN%. NGy9!)W9O",
+    @e.decrypt(my_message, "30218", "12345")
+  end
+
+  def test_it_can_decrypt_many_words_with_comma_period_and_spaces
+     my_message = "the quick brown fox, jumps over the lazy dogs back...end.."
+
+    assert_equal ",f!N9s%.38 >7u)NYmaPxh/(8qN[@c>N,f!N4?cbxb[#.8 9ViOOyc),y9",
+    @e.decrypt(my_message, "30218", "12345")
+  end
+
 end
