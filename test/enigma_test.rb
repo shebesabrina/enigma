c240756ad4eb7593ded670b5e20b565a7051ec81
Lihkin <shniks@gmail.com>
2018-02-04 17:10:01 -0700

    Changing enigma test file.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,45 +12,45 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_if_encrypt_method_returns_a_string
-    assert_equal String, @e.encrypt("hello world", "12345", 30218).class
+    assert_equal String, @e.encrypt("hello world").class
   end
 
   def test_it_can_encrypt_a_letter
     my_message = "k..end.."
 
-    assert_equal "308o658i", @e.encrypt(my_message, "12345", 30218)
+    assert_equal "308o658i", @e.encrypt(my_message)
   end
 
   def test_it_can_encrypt_a_second_letter
     my_message = "ki..end.."
 
-    assert_equal "3 8ixcair", @e.encrypt(my_message, "12345", 30218)
+    assert_equal "3 8ixcair", @e.encrypt(my_message)
   end
 
   def test_it_can_encrypt_a_four_letter_word
     my_message = "bell..end.."
 
-    assert_equal "u6ivr0bxw08", @e.encrypt(my_message, "12345", 30218)
+    assert_equal "u6ivr0bxw08", @e.encrypt(my_message)
   end
 
   def test_it_can_encrypt_two_words
     my_message = "bell hop..end.."
 
-    assert_equal "u6ivq9lzr0bxw08", @e.encrypt(my_message, "12345", 30218)
+    assert_equal "u6ivq9lzr0bxw08", @e.encrypt(my_message)
   end
 
   def test_it_can_encrypt_words_with_a_comma
     my_message = "the answer is, 3..end.."
 
     assert_equal ",9bhtcp6xg7s.17ar0bxw08",
-    @e.encrypt(my_message, "12345", 30218)
+    @e.encrypt(my_message)
   end
 
   def test_it_can_encrypt_many_words_with_comma_period_and_spaces
      my_message = "the quick brown fox, jumps over the lazy dogs back...end.."
 
     assert_equal ",9bh9jfm3z.17lkhydujq.rw8h7yb6oh,9bh42w8q5lq.z.kv,8ir6knr0",
-    @e.encrypt(my_message, "12345", 30218)
+    @e.encrypt(my_message)
   end
 
 end
